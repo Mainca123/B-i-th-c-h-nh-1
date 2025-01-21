c020218da260b538c737ac4187cc66e7ef92090b
Mainca123 <giangchinhtgp@gmail.com>
2025-01-21 15:09:11 +0700

    first commit

---
+++ src/main/java/com/mycompany/myfirstapp/MyFirstApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myfirstapp;
+
+/**
+ *
+ * @author HELLO MAIN_CA
+ */
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
